f5065c9c5e7dc3dc2407cadb3f931d78188b3f4f
codefreak558 <unconfigured@null.spigotmc.org>
2020-04-16 14:27:56 +0200

    Updated dbcofmaker.php and index.php

--- dbconfmaker.php
+++ dbconfmaker.php
@@ -1,22 +1,39 @@
 <?php
-if("dbconfig.php" == true){ //Checks if dbconfig.php exists if it does it echos that the file already exists
-    echo "File already exists";
-} else {
-    $dbfile = fopen("dbconfig.php", "w"); //This creates the dbconfig.php file
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+$host = test_input($_POST["dbhost"]);
+$name = test_input($_POST["dbname"]);
+$user = test_input($_POST["dbuser"]);
+$passwd = test_input($_POST["dbpasswd"]);
 
-    $txt = "<?php\n";
-    fwrite($dbfile, $txt);
-    $txt = "$"."dbhost = '".$_POST['dbhost']."';\n"; // Takes the input from the Host input field and puts it in a variable
-    fwrite($dbfile, $txt);
-    $txt = "$"."dbname = '".$_POST['dbname']."';\n";// Takes the input from the Name input field and puts it in a variable
-    fwrite($dbfile, $txt);
-    $txt = "$"."dbuser = '".$_POST['dbuser']."';\n";// Takes the input from the Username input field and puts it in a variable
-    fwrite($dbfile, $txt);
-    $txt = "$"."dbpasswd = '".$_POST['dbpasswd']."';\n";// Takes the input from the Password input field and puts it in a variable
-    fwrite($dbfile, $txt);
-    $txt = "?>";
-    fwrite($dbfile, $txt);
-    echo "File created";
-    fclose($dbfile);
 }
+
+function test_input($data) { // Gets rid of all tags from the input
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    if(file_exists("dbconfig.php")){ // Checks if dbconfig.php exists if it does it echos that the file already exists
+    echo "File already exists";
+    } else {
+        $dbfile = fopen("dbconfig.php", "w"); // This creates the dbconfig.php file
+
+        $txt = "<?php\n";
+        fwrite($dbfile, $txt);
+        $txt = "$"."dbhost = '".$host."';\n"; // Takes the input from the Host input field and puts it in a variable
+        fwrite($dbfile, $txt);
+        $txt = "$"."dbname = '".$name."';\n";// Takes the input from the Name input field and puts it in a variable
+        fwrite($dbfile, $txt);
+        $txt = "$"."dbuser = '".$user."';\n";// Takes the input from the Username input field and puts it in a variable
+        fwrite($dbfile, $txt);
+        $txt = "$"."dbpasswd = '".$passwd."';\n";// Takes the input from the Password input field and puts it in a variable
+        fwrite($dbfile, $txt);
+        $txt = "?>";
+        fwrite($dbfile, $txt);
+        echo "File created";
+        fclose($dbfile);
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -12,12 +12,30 @@
         
         <!-- This is the form for the database information -->
         <form method="post" action="dbconfmaker.php">
-            <input type="text" name="dbhost" placeholder="Database Host"><br>
-            <input type="text" name="dbname" placeholder="Database Name"><br>
-            <input type="text" name="dbuser" placeholder="Database Username"><br>
-            <input type="password" name="dbpasswd" placeholder="Database Password"><br>
+            <input required type="text" name="dbhost" placeholder="Database Host"><br>
+            <input required type="text" name="dbname" placeholder="Database Name"><br>
+            <input required type="text" name="dbuser" placeholder="Database Username"><br>
+            <input required type="password" name="dbpasswd" placeholder="Database Password"><br>
             <input type="submit" value="submit">
         </form>
+        
+        <?php
+        if($_SERVER["REQUEST METHOD"] == "POST"){
+        $host = test_input($_POST["dbhost"]);
+        $name = test_input($_POST["dbname"]);
+        $user = test_input($_POST["dbuser"]);
+        $passwd = test_input($_POST["dbpasswd"]);
+        
+        }
+            
+        function test_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+        }
+        
+        ?>
 
     </body>
     
